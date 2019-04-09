e70248d494f12ec33663cd9aaf1d7daf8eb6b2c4
Anna Kuokkanen <kuokkanenanna9@gmail.com>
2019-04-09 15:20:03 +0300

    koodia edistetty

--- SnakeGame/src/main/java/GUI/SnakeGraphics.java
+++ SnakeGame/src/main/java/GUI/SnakeGraphics.java
@@ -1,7 +1,6 @@
 
 package GUI;
 
-import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
@@ -10,11 +9,32 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.shape.Polygon;
 import javafx.stage.Stage;
 import com.mycompany.snakegame.Snake;
+import javafx.scene.shape.Circle;
 
 public class SnakeGraphics {
+   
+    private Circle food;
+    private Polygon snake;
     
-    public Polygon getSnake(new Snake(int i, int j)) {
+    public Scene getScene() {
+        Scene scene = new Scene();
         
+        return scene; 
     }
     
+    public void setSnake(int i, int j) {
+        this.snake = new Polygon(i,j,100, 0, 100, 50, 0, 50);
+    }
+    
+    public Polygon getSnake() {
+        return this.snake;
+    }
+    
+    public void setFood(int i, int j) {
+        this.food = new Circle(30, 50, 10);
+    }
+    
+    public Circle getFood() {
+        return this.food;
+    }
 }
