3f4e7454e020d2e0eee42abac31d58f43d81bf15
Anna Kuokkanen <kuokkanenanna9@gmail.com>
2019-04-09 13:53:28 +0300

    koodia edistetty

--- SnakeGame/src/main/java/GUI/FirstView.java
+++ SnakeGame/src/main/java/GUI/FirstView.java
@@ -3,17 +3,19 @@ package GUI;
 
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.FlowPane;
 
 public class FirstView {
     
     public Scene getScene() {
-        
+        TextField field = new TextField();
         Button btn = new Button();
         btn.setText("START");
         
         FlowPane components = new FlowPane();
         components.getChildren().add(btn);
+        components.getChildren().add(field);
         Scene scene = new Scene(components);
         
         return scene;
